1a5eef18f9166656ec132bbb67f9f92a788fca51
ChistyakovDi <111275407+ChistyakovDi@users.noreply.github.com>
2023-02-02 14:26:15 +0300

    Added my project

---
+++ src/main/java/com/chistyakov/GameInfoBot/GameInfoBotApplication.java
@@ -0,0 +1,13 @@
+package com.chistyakov.GameInfoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameInfoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameInfoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chistyakov/infoBot/GameInfoBot/GameInfoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chistyakov.infoBot.GameInfoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameInfoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
